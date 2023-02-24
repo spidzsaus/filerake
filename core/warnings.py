ff77ec13f5dc108d653fb7c0de382c5789a4b0a0
spdz <spidepog@gmail.com>
2023-02-24 13:50:54 +0300

    Added pool functionality

---
+++ core/warnings.py
@@ -0,0 +1,14 @@
+class Warning:
+    def __init__(self, tip=None, **kwargs) -> None:
+        if tip is None:
+            tip = ''
+        self.tip = tip
+        for key, value in kwargs:
+            self.__setattr__(key, value)
+    
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {self.tip}"
+
+
+class FileExistsWarning(Warning):
+    pass
--- pools.py
+++ pools.py
@@ -1,14 +1,27 @@
 from context import __SuggestionTable__
+from pathlib import Path
+from random import randint
+from core.warnings import *
 
 class Pool:
+    name : str
+    path : Path
+
     @classmethod
-    def from_json(cls, dic):
+    def from_json(cls, dic: dict):
         new = cls()
         
         new.name = dic["name"]
-        new.path = dic["path"]
+        new.path = Path(dic["path"])
         __SuggestionTable__.add_rule(new, dic["pattern"])
+        return new
     
-    def send(filepath):
-        ...
+    def send(self, filepath: Path):
+        warnings = set()
+        name = filepath.name
+        if (self.path / name).exists():
+            name = name + str(randint(0, 999999))
+            warn = FileExistsWarning("file exists, added random suffix", filepath=filepath, name=name)
+            warnings.add(warn)
+        filepath.rename(self.path / name)
         
