17b51f43d121157090747bb1ad0fd13a4e84af7c
spdz <spidepog@gmail.com>
2023-02-24 18:13:09 +0300

    Added basic sorter functionality

---
+++ app/raking_session.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from core.pools import Pool
+from .settings import UserSettings
+
+class RakingStep:
+    def __init__(self, file, suggestions, pools):
+        self.file = file
+        self.suggestions = suggestions
+        self.pools = pools
+
+class RakingSession:
+    path : Path
+    settings : UserSettings
+
+    def __init__(self, path : Path, settings : UserSettings):
+        self.path = path
+        self.settings = settings
+        self.pile = (f for f in path.iterdir() if f.is_file())
+        self.prev_file = None
+    
+    def next(self, pool : Pool=None):
+        if self.prev_file is not None:
+            pool.send(self.prev_file)
+        try:
+            file = next(self.pile)
+        except StopIteration:
+            return None
+        
+        name = file.name
+        suggestions = self.settings.suggestion_table.suggest(name)
+        pools = self.settings.pools
+
+        self.prev_file = file
+        return RakingStep(file, suggestions, pools)
--- main.py
+++ main.py
@@ -1,20 +1,55 @@
 from tkinter import filedialog
 import dearpygui.dearpygui as dpg
+from pathlib import Path
 
 from app.settings import UserSettings
+from app.raking_session import RakingSession, RakingStep
 
-__searchPath__ = 'C:/'
-__settings__ = UserSettings('config/settings.json')
+class Context:
+    __searchPath__ = Path('C:/')
+    __settings__ = UserSettings('config/settings.json')
+    __session__ : RakingSession = None
+    __tempPoolsTable__ = {}
 
 def select_path(sender, app_data):
     dir_input = filedialog.askdirectory()
     if not dir_input: return
-    __searchPath__ = dir_input
+    Context.__searchPath__ = Path(dir_input)
     dpg.set_value('main label',
-                  f"Sorting path: {__searchPath__}")
+                  f"Sorting path: {Context.__searchPath__}")
+
+def start_raking(sender, app_data):
+    Context.__session__ = RakingSession(Context.__searchPath__, Context.__settings__)
+    next_raking(..., ...)
+
+def next_raking(sender, app_data):
+    if sender is not Ellipsis:
+        pool = Context.__tempPoolsTable__[int(sender[len('POOL'):])]
+    else:
+        pool = None
+    step = Context.__session__.next(pool)
+    dpg.delete_item('raking')
+    if step:
+        count = 0
+        Context.__tempPoolsTable__.clear()
+        with dpg.child_window(parent='window', tag="raking", label="Raking"):
+            dpg.add_text(f'Reviewing {step.file.name}')
+
+            if step.suggestions:
+                dpg.add_text('Suggested pools:')
+                for sgst in step.suggestions:
+                    dpg.add_selectable(label=sgst.name, callback=next_raking, tag=f"POOL{count}")
+                    Context.__tempPoolsTable__[count] = sgst
+                    count += 1
+                dpg.add_separator()
+            for pool in step.pools:
+                dpg.add_selectable(label=pool.name, callback=next_raking, tag=f"POOL{count}")
+                Context.__tempPoolsTable__[count] = pool
+                count += 1
+
 
 dpg.create_context()
-dpg.create_viewport(title="SortMe!")
+dpg.create_viewport(title="spidz.filerake")
 dpg.setup_dearpygui()
 
 with dpg.font_registry ():
@@ -25,8 +60,10 @@ with dpg.font_registry ():
 dpg.bind_font("cyr")
 
 with dpg.window(tag="window", label="Example Window"):
-    dpg.add_text(f"Sorting path: {__searchPath__}", id='main label')
+    dpg.add_text(f"Sorting path: {Context.__searchPath__}", tag='main label')
     dpg.add_button(label="Change directory", callback=select_path)
+    dpg.add_button(label="Start raking", callback=start_raking)
+
 
 dpg.show_viewport()
 dpg.set_primary_window("window", True)
